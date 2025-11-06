819ee1375b688fe408a51260072ee786868968b9
camiladasilvaa <camidsacosta@gmail.com>
2025-11-05 23:37:06 -0600

    end scene

--- Assets/LaserHit.cs
+++ Assets/LaserHit.cs
@@ -8,13 +8,15 @@ public class LaserHit : MonoBehaviour
         // Option A: By tag
         if (other.CompareTag("Character"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 0f;
         }
 
         // Option B: By layer
         if (other.gameObject.layer == LayerMask.NameToLayer("PlayerBody"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 0f;
         }
     }
 
